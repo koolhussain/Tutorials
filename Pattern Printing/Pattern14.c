5a03ecd380d55c689ed50301d658834e7015eede
koolhussain <ahmad.hussain91@gmail.com>
2018-07-23 10:03:21 +0530

    Commit

---
+++ Pattern Printing/Pattern14.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5-i+1;j++)
+		{
+			printf(" ");
+		}
+		for(k=1;k<=2*i-1;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf(" ");
+		}
+		for(k=1;k<=2*(5-i+1)-1;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
